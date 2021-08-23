7b5edbb4604c8176356fbb8fad79ff1ac5751277
Geovanny Reyes <geovanny.reyes@correounivalle.edu.co>
2021-08-23 13:27:32 -0500

    handle http request

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/styles/components/Main.scss';
+import axios from 'axios';
 
-const Main = () => (
-  <main>
-    <div className='main__content'>
-      <h1>Shortiny</h1>
-      <form action=''>
-        <input type='text' className='input is-rounded is-large' placeholder='Shorten your link' />
-        <button type='submit' className='button is-rounded is-large'>Shorten</button>
-      </form>
-    </div>
-  </main>
-);
+const API = 'http://localhost:3000/';
+
+function Main() {
+  const [data, setData] = useState({ url: '' });
+  function handle(e) {
+    const newdata = { ...data };
+    newdata[e.target.id] = e.target.value;
+    setData(newdata);
+  }
+  function submit(e) {
+    document.getElementById('url').value = '';
+    e.preventDefault();
+    axios.post(API, { url: data.url }).then((res) => {
+      document.getElementById('result_url').innerText = `localhost:3000/l/${res.data.shorturl}`;
+    });
+  }
+
+  return (
+    <main>
+      <div className='main__content'>
+        <h1>Shortiny</h1>
+        <form onSubmit={(e) => submit(e)}>
+          <input onChange={(e) => handle(e)} value={data.url} type='text' className='input is-rounded is-large' id='url' name='url' placeholder='Shorten your link' />
+          <button type='submit' id='btn' className='button is-rounded is-large'>Shorten</button>
+        </form>
+        <div className='div__result'><p id='result_url'> </p></div>
+      </div>
+    </main>
+  );
+}
 
 export default Main;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,8 +3,6 @@ import '../assets/styles/App.scss';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
 
-// const API = 'http://localhost:3000/';
-
 const App = () => {
   return (
     <div className='App'>
